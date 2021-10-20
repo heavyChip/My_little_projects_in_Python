59db14206366e5db7d75f145b771acd8501abd3f
heavyChip <heavyChip@outlook.com>
2021-10-20 12:45:15 +0200

    Changes

--- Num_checker_1.py
+++ Num_checker_1.py
@@ -1,20 +1,23 @@
 import re
 
 def show_differences_between_sets():
-    # Function returns (in a set) differences between two sets of numbers
+    """
+    Function which returns (in a set) differences between two sets of numbers (one fixed and second typed into the console).
+    """
 
     pattern_sett = set(list(range(11)))
-    #pattern_sett = {0,1,2,3,4,5,6,7,8,9,10}
+    # pattern_sett = {0,1,2,3,4,5,6,7,8,9,10}
+
     temp_entered_numbers = input("Enter numbers from 0 to 10 in order to create a set: ")
 
     pattern = re.compile(r"(10|[0-9])")
     temp_entered_numbers = pattern.findall(temp_entered_numbers)
 
     entered_numbers_sett = set(map(int, temp_entered_numbers))
-    #print(entered_numbers_sett)
+    # print(entered_numbers_sett)
 
     differences_sett = pattern_sett.difference(entered_numbers_sett)
-    #print(list(differences_sett))
+    # print(list(differences_sett))
 
     return differences_sett
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # My_little_projects_in_Python
 
 List of my own little projects:
-  1. Num_checker_1 - Function which returns differences between two sets of numbers {0 - 10}
+  1. Num_checker_1 - Function which returns (in a set) differences between two sets of numbers (one fixed {0-10} and second typed into the console).
